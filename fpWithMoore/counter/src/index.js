081afad98fb85a7064bff11972360b34607f232e
Adrian <adrian.zawadzki@outlook.com>
2019-01-26 14:22:20 +0100

    final commit - added Virtual DOM library

--- fpWithMoore/counter/src/index.js
+++ fpWithMoore/counter/src/index.js
@@ -1,5 +1,7 @@
-import h from 'hyperscript';
+// import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { div, button } = hh(h);
 
@@ -9,38 +11,44 @@ function view(dispatch, model) {
     return div([
         div({ className: 'mv2' }, `Count: ${model}`),
         button({ className: 'pv1 pv2 mr2', 
-            onclick: () => dispatch('plus') }, '+'),
+            onclick: () => dispatch(MSGS.ADD) }, '+'),
         button({ className: 'pv1 pv2',
-            onclick: () => dispatch('minus') }, '-'),
+            onclick: () => dispatch(MSGS.SUBTRACT) }, '-'),
     ]);
+};
+
+const MSGS = {
+    ADD: 'ADD',
+    SUBTRACT: 'SUBTRACT',
 };
 
 function update(msg, model) {
     switch (msg) {
-        case 'plus':
+        case MSGS.ADD:
             return model + 1;
-        case 'minus':
+        case MSGS.SUBTRACT:
             return model -1;
         default:
             return model;
     }
-}
+};
 
 // impure code below 
 
 function app(initModel, update, view, node) {
     let model = initModel;
     let currentView = view(dispatch, model);
-    node.appendChild(currentView);
+    let rootNode = createElement(currentView);
+    node.appendChild(rootNode);
 
     function dispatch(msg) {
         model = update(msg, model);
         const updatedView = view(dispatch, model);
-        node.replaceChild(updatedView, currentView);
+        const patches = diff(currentView, updatedView);
+        rootNode = patch(rootNode, patches);
         currentView = updatedView;
-    }
-
-}
+    };
+};
 
 const rootNode = document.getElementById('app');
 
