659cf9705ee6c9a73594e5678005f8638b7333c5
Adrian <adrian.zawadzki@outlook.com>
2019-02-04 12:03:18 +0100

    update file

--- fpWithMoore/TipCalcApp/src/Update.js
+++ fpWithMoore/TipCalcApp/src/Update.js
@@ -1,7 +1,38 @@
 import * as R from 'ramda';
 
+
+const MSGS = {
+  BILL_AMOUNT_INPUT: 'BILL_AMOUNB_INPUT',
+  TIP_PERCENT_INPUT: 'TIP_PERCENT_INPUT',
+};
+
+function billAmountInputMsg(billAmount) {
+  return {
+    type: MSGS.BILL_AMOUNT_INPUT,
+    billAmount
+  };
+}
+
+function billPercentInputMsg(tipPercent) {
+  return {
+    type: MSGS.TIP_PERCENT_INPUT,
+    tipPercent
+  };
+}
+
 function update (msg, model) {
-  return model;
+  switch (msg.type) {
+    case MSGS.BILL_AMOUNT_INPUT: {
+      const { billAmount } = msg;
+      return { ...model, billAmount };
+    }
+    case MSGS.TIP_PERCENT_INPUT: {
+      const { tipPercent } = msg;
+      return { ...model, tipPercent };
+    }
+    default:
+      return model;
+  }
 }
 
 export default update;
