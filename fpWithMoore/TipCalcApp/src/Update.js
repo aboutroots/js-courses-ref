26f30fb90458e45b9a11fef0e98cd95ba3c20547
Adrian <adrian.zawadzki@outlook.com>
2019-02-04 12:18:14 +0100

    final commit

--- fpWithMoore/TipCalcApp/src/Update.js
+++ fpWithMoore/TipCalcApp/src/Update.js
@@ -1,26 +1,23 @@
-import * as R from 'ramda';
-
-
-const MSGS = {
-  BILL_AMOUNT_INPUT: 'BILL_AMOUNB_INPUT',
+export const MSGS = {
+  BILL_AMOUNT_INPUT: 'BILL_AMOUNT_INPUT',
   TIP_PERCENT_INPUT: 'TIP_PERCENT_INPUT',
 };
 
-function billAmountInputMsg(billAmount) {
+export function billAmountInputMsg(billAmount) {
   return {
     type: MSGS.BILL_AMOUNT_INPUT,
-    billAmount
+    billAmount,
   };
 }
 
-function billPercentInputMsg(tipPercent) {
+export function tipPercentInputMsg(tipPercent) {
   return {
     type: MSGS.TIP_PERCENT_INPUT,
-    tipPercent
+    tipPercent,
   };
 }
 
-function update (msg, model) {
+function update(msg, model) {
   switch (msg.type) {
     case MSGS.BILL_AMOUNT_INPUT: {
       const { billAmount } = msg;
