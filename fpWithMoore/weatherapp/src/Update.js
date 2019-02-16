2919aaade92e11520d35923a216d2ba6d9aa8038
Adrian <adrian.zawadzki@outlook.com>
2019-02-16 17:40:03 +0100

    putting the side effects in functional programming style

--- fpWithMoore/weatherapp/src/Update.js
+++ fpWithMoore/weatherapp/src/Update.js
@@ -4,7 +4,16 @@ export const MSGS = {
   LOCATION_INPUT: 'LOCATION_INPUT',
   ADD_LOCATION: 'ADD_LOCATION',
   REMOVE_LOCATION: 'REMOVE_LOCATION',
+  HTTP_SUCCESS: 'HTTP_SUCCESS',
 };
+
+const APPID = 'f4040baa907e068e29360361cc63d9ae';
+
+function weatherUrl(city) {
+  return `http://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+    city,
+    )}&units=imperial&APPID=${APPID}`;
+}
 
 export function locationInputMsg(location) {
   return {
@@ -24,6 +33,12 @@ export function removeLocationMsg(id) {
   };
 }
 
+const httpSuccessMsg = R.curry((id, response) => ({
+  type: MSGS.HTTP_SUCCESS,
+  id,
+  response,
+}));
+
 function update(msg, model) {
   switch (msg.type) {
     case MSGS.LOCATION_INPUT: {
@@ -40,18 +55,48 @@ function update(msg, model) {
         high: '?',
       };
       const updatedLocations = R.prepend(newLocation, locations);
-      return {
+      return [{
         ...model,
         location: '',
         locations: updatedLocations,
         nextId: nextId + 1,
-      };
+      },
+      {
+        request: { url: weatherUrl(location) },
+        successMsg: httpSuccessMsg(nextId),
+      }
+    ];
     }
     case MSGS.REMOVE_LOCATION: {
       const { id } = msg;
       const { locations } = model;
       const updatedLocations = R.reject(R.propEq('id', id), locations);
       return { ...model, locations: updatedLocations };
+    }
+
+    case MSGS.HTTP_SUCCESS: {
+      const { id, response } = msg;
+      const { locations } = model;        // response.data.main 
+      const { temp, temp_min, temp_max } = R.pathOr(
+        {},
+        ['data', 'main'],
+        response,
+      );
+      const updatedLocations = R.map(location => {
+        if (location.id === id) {
+          return {
+            ...location,
+            temp: Math.round(temp),
+            low: Math.round(temp_min),
+            high: Math.round(temp_max),
+          };
+        }
+        return location;
+      }, locations);
+      return {
+        ...model,
+        locations: updatedLocations,
+      };
     }
   }
   return model;
