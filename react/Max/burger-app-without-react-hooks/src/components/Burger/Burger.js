0dfbcf8916394e18d924154c83aeb52ebda691d2
Adrian <adrian.zawadzki@outlook.com>
2019-03-14 12:10:27 +0100

    dynamically burger

--- react/Max/burger-app-without-react-hooks/src/components/Burger/Burger.js
+++ react/Max/burger-app-without-react-hooks/src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const transformedIngredients  = Object.keys(props.ingredients)  // to transformuje obiekt z parami klucz-wartość, w tablicę 
+    .map(igKey => {                                               // składników burgera, gdzie na podstawie wartości obiektu decyduje            
+      return [...Array(props.ingredients[igKey])].map((_, i) => { // jak wiele składników potrzebuję. Klucz potrzebny by wiedzieć, jakiego
+        return <BurgerIngredient key={igKey + 1} type={igKey} />  // typu składników potrzebuję
+      });
+    });
+
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
