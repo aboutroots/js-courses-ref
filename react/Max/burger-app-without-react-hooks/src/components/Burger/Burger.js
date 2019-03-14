613e68261dffdae9527c78206224009211b695e1
Adrian <adrian.zawadzki@outlook.com>
2019-03-14 14:04:48 +0100

    connecting state to build controls

--- react/Max/burger-app-without-react-hooks/src/components/Burger/Burger.js
+++ react/Max/burger-app-without-react-hooks/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients  = Object.keys(props.ingredients)  // to transformuje obiekt z parami klucz-wartość, w tablicę 
+  let transformedIngredients  = Object.keys(props.ingredients)  // to transformuje obiekt z parami klucz-wartość, w tablicę 
     .map(igKey => {                                               // składników burgera, gdzie na podstawie wartości obiektu decyduje            
       return [...Array(props.ingredients[igKey])].map((_, i) => { // jak wiele składników potrzebuję. Klucz potrzebny by wiedzieć, jakiego
-        return <BurgerIngredient key={igKey + 1} type={igKey} />  // typu składników potrzebuję
+        return <BurgerIngredient key={igKey + i} type={igKey} />  // typu składników potrzebuję
       });
-    });
+    })
+    .reduce((err, el) => {
+      return err.concat(el)
+    }, []);
+    console.log(transformedIngredients)
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return(
     <div className={classes.Burger}>
