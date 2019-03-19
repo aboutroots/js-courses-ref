402639e1883f41a0a2e2fab5285fc1841d224655
Adrian <adrian.zawadzki@outlook.com>
2019-03-19 16:15:39 +0100

    post-error-delete

--- react/Max/httpAndAjax/src/containers/Blog/Blog.js
+++ react/Max/httpAndAjax/src/containers/Blog/Blog.js
@@ -10,6 +10,7 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
+        error: false,
     }
 
     componentDidMount() {
@@ -24,6 +25,9 @@ class Blog extends Component {
                 })
                 this.setState({posts: updatedPosets});
                 //console.log(resopnse)
+            })
+            .catch(error => {
+                this.setState({error: true})
             });
     }
 
@@ -32,13 +36,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
 
         return (
             <div>
