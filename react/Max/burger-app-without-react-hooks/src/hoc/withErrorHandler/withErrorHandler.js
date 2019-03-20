fad37b52acbdf91eefc5b3fd34fa7b65eb099adc
Adrian <adrian.zawadzki@outlook.com>
2019-03-20 21:08:32 +0100

    comments for error handling

--- react/Max/burger-app-without-react-hooks/src/hoc/withErrorHandler/withErrorHandler.js
+++ react/Max/burger-app-without-react-hooks/src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,17 +4,17 @@ import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Auxiliary/Auxiliary';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-  return class extends Component {
+  return class extends Component {             // To wzorzec fabryki klasy
     state = {
       error: null,
     }
 
     componentDidMount() {
-      axios.interceptors.request.use(req => {
-        this.setState({error: null});
+      axios.interceptors.request.use(req => {   // w instancji axios mogę ustawić globalny interceptor, który pozwala
+        this.setState({error: null});           // obsłużyć błędy /////////  PODCZAS WYSYŁANIA ZAPYTANIA
         return req;
       })
-      axios.interceptors.response.use(res => res, error => {
+      axios.interceptors.response.use(res => res, error => {      ///////// PODCZAS ODBIERANIA ODPOWIEDZI
         this.setState({error: error});
       })
     }
@@ -27,9 +27,9 @@ const withErrorHandler = (WrappedComponent, axios) => {
       return (
         <Aux>
           <Modal 
-            show={this.state.error}
-            clicked={this.errorConfirmedHandler} >
-            {this.state.error ? this.state.error.message : null}
+            show={this.state.error}   // Czyli gdy error nie jest null. Poniższe message to właściwość error zwrócona przez Firebase.
+            modalClosed={this.errorConfirmedHandler} >  
+            {this.state.error ? this.state.error.message : null}   
           </Modal>
           <WrappedComponent {...this.props} />
         </Aux>
