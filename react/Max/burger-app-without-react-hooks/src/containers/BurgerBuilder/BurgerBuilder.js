be2985bb4b41c8c327c8cceb2dd9b28064f97077
Adrian <adrian.zawadzki@outlook.com>
2019-04-01 19:11:18 +0200

    adding routing

--- react/Max/burger-app-without-react-hooks/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react/Max/burger-app-without-react-hooks/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,28 +88,18 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Adam Roman',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '32-132',
-          counter: 'Poland'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest',
-    }
-    axios.post('/orders.json', order)
-      .then(resposne => {
-        this.setState({ loading: false, purchasing: false })
+    
+      const queryParams = [];
+      for (let i in this.state.ingredients) {
+        queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+      }
+
+      queryParams.push('price=' + this.state.totalPrice)
+      const queryString = queryParams.join('&');
+      this.props.history.push({
+        pathname: '/checkout',
+        search: '?' + queryString,
       })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-      });
   }
 
   render() {
