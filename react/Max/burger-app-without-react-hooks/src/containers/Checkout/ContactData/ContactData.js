f2b0d78576f91ed4ada6a224af6ca622194e2efb
Adrian <adrian.zawadzki@outlook.com>
2019-04-02 14:42:59 +0200

    cloning deeply the state

--- react/Max/burger-app-without-react-hooks/src/containers/Checkout/ContactData/ContactData.js
+++ react/Max/burger-app-without-react-hooks/src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,61 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP CODE',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-Mail',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'},
+          ]
+        },
+        value: '',
+      },
     },
     loading: false,
   }
@@ -23,16 +70,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
-      customer: {
-        name: 'Adam Roman',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '32-132',
-          counter: 'Poland'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest',
     }
     axios.post('/orders.json', order)
       .then(resposne => {
@@ -44,13 +81,38 @@ class ContactData extends Component {
       });
   }
 
+  inputChangeHandler = (event, inputIdentifier) => {
+    // cloning deeply
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
+
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+
     let form = (
       <form>
-        <input className={classes.Input} type='text' name='name' placeholder='your name' />
-        <input className={classes.Input} type='email' name='email' placeholder='your name' />
-        <input className={classes.Input} type='text' name='street' placeholder='street' />
-        <input className={classes.Input} type='text' name='postal' placeholder='postal code' />
+        {formElementsArray.map(formElement => (
+          <Input 
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangeHandler(event, formElement.id)} />
+        ))}
         <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
