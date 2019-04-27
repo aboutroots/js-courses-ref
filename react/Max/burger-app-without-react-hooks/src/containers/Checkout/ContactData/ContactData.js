2e2c54bcf0da57e7062b177e71c7dae01e8e4105
Adrian <adrian.zawadzki@outlook.com>
2019-04-27 21:22:24 +0200

    redirect to improve UX

--- react/Max/burger-app-without-react-hooks/src/containers/Checkout/ContactData/ContactData.js
+++ react/Max/burger-app-without-react-hooks/src/containers/Checkout/ContactData/ContactData.js
@@ -93,7 +93,6 @@ class ContactData extends Component {
       },
     },
     formIsValid: false,
-    loading: false,
   }
 
   orderHandler = (event) => {
@@ -176,7 +175,7 @@ class ContactData extends Component {
         <Button disabled={!this.state.formIsValid} btnType='Success' >ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />
     }
     return (
@@ -192,13 +191,14 @@ const mapStateToProps = state => {
   return {
     ings: state.ingredients,
     price: state.totalPrice,
+    loading: state.loading,
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
   }
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
