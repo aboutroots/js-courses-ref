8f1c98015abd72159cf7ecb0f484cf5b561f5f26
Adrian <adrian.zawadzki@outlook.com>
2019-04-30 21:40:06 +0200

    Getting a Token from the Backend

--- react/Max/burger-app-without-react-hooks/src/store/actions/auth.js
+++ react/Max/burger-app-without-react-hooks/src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -13,7 +15,7 @@ export const authSuccess = (authData) => {
   };
 };
 
-export const authError = (error) => {
+export const authFail = (error) => {
   return {
     type: actionTypes.AUTH_FAIL,
     error: error,
@@ -24,5 +26,19 @@ export const auth = (email, password) => {
   // here is authorization the user
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA9E6l54nPtvm8BzTYmIbsDEufTmvkxgcY', authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
